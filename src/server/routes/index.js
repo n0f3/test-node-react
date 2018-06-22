4b42260639cd63eb7ac5dcc405203b24b49b8584
Alessandro Metta <ametta@mtechnologies.io>
2018-06-21 19:34:31 -0700

    Completed first end point

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const auth = require('./auth');
-// const fileManagement = require('./fileManagement');
-// const { sendEmail } = require('../sparkPost');
-// const router = express.Router();
-// const rootPath = process.cwd();
-
-// const { getApartments, createApartment, getSingleApartment, modifyApartment, deleteApartment } = require('./apartments');
-
-// router.get('/parse', )
-
-// module.exports = router;
-
 import express from 'express';
 import parse from './parse';
 
--- src/server/routes/parse.js
+++ src/server/routes/parse.js
@@ -1,21 +1,34 @@
 import request from 'request';
+import cheerio from 'cheerio';
 
 const parse = (req, res, next) => {
   console.log(req.query);
   let endpoint = req.query.endpoint;
-  const tag = req.query.tag;
-  if (endpoint && tag) {
+  const tags = req.query.tag;
+  if (endpoint && tags) {
     const httpRegex = /^(https?):\/\//g
     const hasHttp = httpRegex.test(endpoint);
     if (!hasHttp) {
       endpoint = `http://${endpoint}`;
     }
-    request(endpoint, (error, response, body) => {
+    request(endpoint, (error, response, html) => {
       if (error) {
         return next(error);
       }
-      console.log(response);
-      console.log(body);
+      const $ = cheerio.load(html);
+      const result = {};
+      tags.forEach((tag) => {
+        $(tag).each((i, element) => {
+          if (!result[tag]) {
+            result[tag] = [];
+          }
+          result[tag].push({
+            innerText: $(element).text(),
+            innerHTML: $(element).html(),
+          });
+        });
+      });
+      res.json(result);
     });
   } else {
     res.status(400).send('Missing query param, both tag and endpoint are required');

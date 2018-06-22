905238620f688ea52286f32d4aad28653f54d6e4
Alessandro Metta <ametta@mtechnologies.io>
2018-06-21 18:32:58 -0700

    Modified eslint rule, fixed error handling

--- src/server/configApp.js
+++ src/server/configApp.js
@@ -23,13 +23,14 @@ const configApp = (app) => {
   });
 
   // catch 404 and forward to error handler
-  app.use((req, res) => {
-    res.status(404).send('Not Found');
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   });
 
   // error handler
   app.use((err, req, res, next) => {
-    console.log(`error: ${err}`);
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
